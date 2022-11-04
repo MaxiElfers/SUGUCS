26830978b176e8b4f7637a7207adefab5bc7017d
Maximilian Elfers <maximilian-elfers@gmx.de>
2022-11-04 22:51:47 +0100

    added two calibration functions

--- kalibrierung.js
+++ kalibrierung.js
@@ -1,30 +1,77 @@
-let btn_Kalibrierung = document.getElementById("btn_Kalibrierung");
-let btn_click = btn_Kalibrierung.addEventListener("click", function(){startSystem();});
+let btn_calibration = document.getElementById("btn_Kalibrierung");
+let btn_click = btn_calibration.addEventListener("click", function(){startSystem();});
 
-let referenceDaten = [10, 40];
-let gelieferteDaten = [5, 34];
-let newData = [];
+let referenceDaten1 = [10, 40];
+let referenceDaten2 = [4, 7];
+let referenceDaten3 = [20, 31];
+let gelieferteDaten1 = [5, 34];
+let gelieferteDaten2 = [5, 10];
+let gelieferteDaten3 = [24, 30];
+let singleDelta = 0;
+let multiDelta = [];
 
+/**
+ * function starts the system
+ */
 function startSystem(){
     // to-do: Wert bekommen
 
-    kalibrierung();
-    console.log(newData)
+    calibration();
+    console.log()
 }
 
-function kalibrierung(){
-    var reference = ArrayAvg(referenceDaten);
-    var geliefert = ArrayAvg(gelieferteDaten);
+/**
+ * Starts the calibration process
+ * Maybe you can choose the method of the
+ * calibration
+ */
+function calibration(){
+    let lieferStack = [gelieferteDaten1, gelieferteDaten2, gelieferteDaten3];
+    let referStack = [referenceDaten1, referenceDaten2, referenceDaten3];
+    multiReferenceDeltaKali(referStack, lieferStack );
+}
 
-    var delta = reference - geliefert
+/**
+ * This function averages the check array
+ * and the array to calibrate and calculates 
+ * one single delte which then will be used to
+ * add onto the whole array
+ */
+function oneDeltaKali(){
+    let reference = ArrayAvg(referenceDaten);
+    let geliefert = ArrayAvg(gelieferteDaten);
 
+    singleDelta = reference - geliefert;
+}
+
+/**
+ * This function calculates an Array of deltas,
+ * which then can be added to all the collected 
+ * data to calibrate them
+ * For this function the sound must be exactly 
+ * the same lenght every time
+ */
+function multiDeltaKali(){
     gelieferteDaten.forEach((Wert, index) => {
-        newData[index] = Wert + delta;
-    });
+        multiDelta[index] = referenceDaten[index] - Wert;
+    })
+}
+
+function multiReferenceDeltaKali(referenceStack, geliefertStack){
+    let deltaArray = [];
+    let hilfsArray = [];
+    geliefertStack.forEach((Daten, index) => {
+        let hilfsArrayReference = referenceStack[index];
+        Daten.forEach((dat, index) => {
+            hilfsArray[index] = hilfsArrayReference[index] - dat;
+        })
+        deltaArray[index] = hilfsArray;
+    })
+    console.log(deltaArray)
 }
 
 function ArrayAvg(myArray) {
-    var i = 0, summ = 0, ArrayLen = myArray.length;
+    let i = 0, summ = 0, ArrayLen = myArray.length;
     while (i < ArrayLen) {
         summ = summ + myArray[i++];
     }
