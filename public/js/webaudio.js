fe55c92cc0a50ed0d4934214cfcba7676ecda4c4
Lorano98 <evalangstein@freenet.de>
2022-11-16 17:28:00 +0100

    Revert "Merge branch 'App' of https://github.com/MaxiElfers/SUGUCS into App"
    This reverts commit dd042ae630432fa3dcd4551533559f71c8c496c3, reversing
    changes made to bb49a9ee4234c39d958bb920e5831639dbf3321f.

--- public/js/webaudio.js
+++ public/js/webaudio.js
@@ -1,9 +1,10 @@
 var localDbValues = []; // array to store db values for each loop withing the refresh_rate
-var refresh_rate = 5000;
+var refresh_rate = 500;
+var color = "green";
 var stream;
+var offset = 30;
 var average = 0;
-
-const db = document.getElementById("db");
+var date;
 
 messungButton = document.getElementById("messung");
 messungButton.addEventListener("click", startMessung);
@@ -52,6 +53,7 @@ function startMessung() {
   var updateDb = function () {
     window.clearInterval(interval);
 
+    const db = document.getElementById("db");
     var volume = Math.round(
       localDbValues.reduce((a, b) => a + b) / localDbValues.length
     );
