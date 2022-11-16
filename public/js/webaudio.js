5e6568bfdd3c056f2cbf3996604e9de86ad3d523
luening <he-luening@t-online.de>
2022-11-16 17:38:55 +0100

    offset added

--- public/js/webaudio.js
+++ public/js/webaudio.js
@@ -1,6 +1,7 @@
 var localDbValues = []; // array to store db values for each loop withing the refresh_rate
 var refresh_rate = 5000;
 var stream;
+var offset = 30;
 var average = 0;
 
 const db = document.getElementById("db");
@@ -18,7 +19,7 @@ function startMessung() {
       const analyser = context.createAnalyser();
 
       analyser.smoothingTimeConstant = 0.8;
-      analyser.fftSize = 256;
+      analyser.fftSize = 256; 
 
       source.connect(analyser);
       analyser.connect(processor);
@@ -34,7 +35,7 @@ function startMessung() {
           values += data[i];
         }
 
-        average = 20 * Math.log10(values / data.length);
+        average = 20 * Math.log10(values / data.length)+offset;
         if (isFinite(average)){
           db.innerText = average;
           localDbValues.push(average);
@@ -42,22 +43,4 @@ function startMessung() {
         
       };
     });
-
-
-  // update the volume every refresh_rate m.seconds
-  var updateDb = function () {
-    window.clearInterval(interval);
-
-    var volume = Math.round(
-      localDbValues.reduce((a, b) => a + b) / localDbValues.length
-    );
-    //var volume = Math.round(Math.max.apply(null, localDbValues));
-    if (!isFinite(volume)) volume = 0; // we don't want/need negative decibels in that case
-    db.innerText = volume;
-    localDbValues = []; // clear previous values
-
-    changeUpdateRate();
-    interval = window.setInterval(updateDb, refresh_rate);
-  };
-  var interval = window.setInterval(updateDb, refresh_rate);
 }
