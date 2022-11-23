85de8415ba33c15ae3a83d3280b1c7d1c51759a5
rob2top <rschmit1@wwu.de>
2022-11-23 11:30:16 +0100

    QRCode weiter Kommentiert

--- public/js/qrcode.js
+++ public/js/qrcode.js
@@ -17,6 +17,11 @@ function scanneQR(){
  * Funktion erstellt eine Zufällige sechsstellige Zahlenfolge, welche als Gruppencode fungiert.
  * Erstellt im selben Zug einen QR Code, der diese Zahlenfolge repräsentiert. (ggf. mit link, der direkt auf die Website führt)
  * Zeigt QR Code unter dem Button Gruppe hosten an. 
+ * ?? erstellt Gruppe und Gruppencode in Datenbank ??
+ * 
+ * Liste mit Teilnehmenden der Gruppe (print unter QR Code)
+ * Startpunkt der Kalibrierung (? Countdown zu Start ?)
+ * Check ob alle Teilnehmer bereit (print hinter Teilnehmer)
  */
 function erstelleGruppe(){
 
