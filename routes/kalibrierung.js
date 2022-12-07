8cafe67abd86aafa0c35d28ec16136fd6e634813
MaxiElfers <91053215+MaxiElfers@users.noreply.github.com>
2022-12-07 10:18:57 +0100

    Revert "Maxis branch"

--- routes/kalibrierung.js
+++ routes/kalibrierung.js
@@ -6,12 +6,4 @@ router.get("/", function (req, res, next) {
   res.render("kalibrierung", { title: "Kalibrierung" });
 });
 
-router.get("/group-lead", function (req, res, next) {
-  res.render("kal-group-lead", { title: "Kalibrierung" });
-});
-
-router.get("/group", function (req, res, next) {
-  res.render("kal-group", { title: "Kalibrierung" });
-});
-
 module.exports = router;
