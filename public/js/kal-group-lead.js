b41427bbfd0e8557c513cb522917369b89083e60
Maximilian Elfers <maximilian-elfers@gmx.de>
2023-01-10 15:56:04 +0100

    cleaned up the code

--- public/js/kal-group-lead.js
+++ public/js/kal-group-lead.js
@@ -1,3 +1,4 @@
+/***** all getElements *****/
 let btn_calibration = document.getElementById("btn_Kalibrierung");
 let input_GroupCode = document.getElementById("input_GroupCode");
 let output_error = document.getElementById("output_Error");
@@ -6,13 +7,17 @@ let in_file = document.getElementById("in_file");
 let btn_xl2 = document.getElementById("btn_XL2");
 let in_soundArray = document.getElementById("soundArray")
 
+/***** all EventListeners ******/
 btn_Gcal.addEventListener("click", function(){checkError("group");});
 btn_xl2.addEventListener("click", function(){window.location = '/kalibrierung/XL2';});
 
-const audio_steig = new Audio('/sounds/DB_steigend.mpeg');
-const audio_const = new Audio('/sounds/DB_konstant.mpeg');
-const audio_schwan = new Audio('/sounds/DB_schwankend.mpeg');
+/***** all Variables ******/
+const audio_calibration = new Audio('/sounds/Calibration_sound.mpeg');
+let xl2Tonspur = in_soundArray.innerHTML.split(',');
+let counter = 0;
 
+
+/***** all functionalities ******/
 
 /**
  * tests that the preconditions are given, so 
@@ -54,92 +59,19 @@ function checkError(art){
  * if button "Sound Starten" is clicked
  */
 function playSound(){
-    audio_schwan.play();
+    audio_calibration.play();
 }
 
-let referenceDaten = [10, 40];
-let gelieferteDaten = [4, 42];
-let allDeltas = [];
-let soundDatei;
-let xl2Tonspur = in_soundArray.innerHTML.split(',');
-var counter = 0;
 
+// @ToDo: Funktion übersichtlicher schreiben und ordentliche Error behandlung einbauen
 /**
- * Takes the soundArray of the xl2 and the soundArray of
- * the device used by the user and compares them. The differences
- * in different decible heights will be taken the mean and that will 
- * be the delta for this height.
- * @param {Array} xl2Array - soundArray of the xl2
- * @param {Array} userArray - soundArray of the users device
- * @returns allDeltas - an Array of Deltas for different decibles
+ * Finds the maximum db value from the given
+ * sound array
+ * @param {Array} tonspur 
  */
-function calibration(xl2Array, userArray){
-    return;
-}
-
-/**
- * This function calculates an Array of deltas,
- * which then can be added to all the collected 
- * data to calibrate them
- * For this function the sound must be exactly 
- * the same lenght every time
- */
-function multiDeltaKali(){
-    gelieferteDaten1.forEach((Wert, index) => {
-        multiDelta[index] = referenceDaten1[index] - Wert;
-    })
-}
-
-
-/**
- * This function adds the calculated Delta to the data of 
- * a single calibration or groupcalibration
- * @param {*} delta 
- * @param {*} data 
- */
-function deltaAnwenden(delta, data){
-    if(delta === "single"){
-        data.forEach((number,index) => {
-            data[index] = number + singleDelta;
-        })
-    }
-    else{
-        data.forEach((number, index) => {
-            data[index] = number + multiDelta[index]; 
-        })
-    }
-}
-
-
-function getAudioFile(){
-    var reader = new FileReader();
-    reader.onload = (event) => {
-        soundDatei = event.target.result;
-        console.log(soundDatei);
-    };
-    reader.readAsArrayBuffer(in_file.files[0]);
-}
-
-async function getAudio(){
-    let audioData = await fetch('/sounds/Testaudio_XL2.wav').then(r => r.arrayBuffer());
-    let audioCtx = new AudioContext({sampleRate:44100});
-    let decodedData = await audioCtx.decodeAudioData(audioData); // audio is resampled to the AudioContext's sampling rate
-    console.log(decodedData.length, decodedData.duration, decodedData.sampleRate, decodedData.numberOfChannels);
-    let float32Data = decodedData.getChannelData(0); // Float32Array for channel 0
-    
-    console.log(float32Data);
-}
-
-
-
-///////////////////////////////////////////////////////////////////////////
-//// Array kürzen
-///////////////////////////////////////////////////////////////////////////
-
-// Tonspur Startton(Maximum) finden
 function tonspurMax(tonspur) {
     console.log("Array Laenge ist: " + tonspur.length)
-  
+
     // Maximum berechnen 
     // überprüfen von Array
     if (tonspur.length === 0) {
@@ -155,7 +87,7 @@ function tonspurMax(tonspur) {
         }
     }
     console.log("Max Index ist: " + maxIndex)
-  
+
     var realMaxIndex = maxIndex
     // gucken, dass es wirklich der letzte aufgenommene dB-Wert des Starttons ist
     for(i = maxIndex + 1; i < maxIndex + 10; i++) { // 10 als Zeiteinheit für maximale Länge des Starttons 
@@ -164,19 +96,23 @@ function tonspurMax(tonspur) {
         }
     }
     console.log("Real Max Index ist: " + realMaxIndex)
-  
+
     // überprüfen ob Array groß genug ist bzw. ganze Zeit aufgenommen hat
     if ( tonspur.length - realMaxIndex + 30 > 0 ) { // 30 Testzeiteinheit für zu kalibrierendes Audio
         tonspurKuerzen(realMaxIndex, tonspur)
     } else {
         console.log("Aufnahme ist zu kurz")
     }
-  }
+}
   
-  // Tonspur kürzen
-  function tonspurKuerzen(max, tonspur) {
-    console.log("Bereit zum kuerzen")
-    // Array kürzen auf richtige Länge
-    tonspur = tonspur.slice(max, max + 30)
-    console.log(tonspur)
-  }
+/**
+ * Shortens the sound array to match the length of the recorded one
+ * @param {Number} max 
+ * @param {Array} tonspur 
+ */
+function tonspurKuerzen(max, tonspur) {
+  console.log("Bereit zum kuerzen")
+  // Array kürzen auf richtige Länge
+  tonspur = tonspur.slice(max, max + 30)
+  console.log(tonspur)
+}
