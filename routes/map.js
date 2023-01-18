cb9956535701d7b0e7c86591763ece6e6eb2573a
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2023-01-18 14:03:26 +0100

    now working connection to plumber

--- routes/map.js
+++ routes/map.js
@@ -1,13 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+/*var SID = document.getElementById("sid").value;
+var starttime = document.getElementById("starttimeInput").value;
+var endtime = document.getElementById("endtimeInput").value;
+var SBID = document.getElementById("userInput").value;
+*/
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
+
     res.render("map", {
         title: "Karte",
-        ueblink: "http://localhost:8000/linearRegression",
-        weblink: "http://localhost:8000/Scatter"
+        weblink: "http://127.0.0.1:8000/structurePlot?boxID=60f077874fb91e001c71b3b1&from=2022-11-22T08%3A00%3A00Z&to=2022-11-23T08%3A00%3A00Z&Day=FALSE&Night=TRUE&MA=TRUE&Mean=TRUE&colors=1&theme=1",
     });
 });
+
 
 module.exports = router;
